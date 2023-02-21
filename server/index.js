1bb6b71d0532c6bee3e1c55990a0904abeaa2311
Matt Cha <mcha002@ucr.edu>
2023-02-21 19:49:23 +0000

    use SQL to insert the data into the database  Query the database and then send the result back to the server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,15 @@ require('dotenv/config');
 const express = require('express');
 const staticMiddleware = require('./static-middleware');
 const errorMiddleware = require('./error-middleware');
+const ClientError = require('./client-error');
+const pg = require('pg');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 const app = express();
 
@@ -13,10 +22,32 @@ app.get('/api/hello', (req, res) => {
 
 app.use(errorMiddleware);
 
-/* app.post('api/events/', (req, res) => {
-  const name = req.body.name;
-  const details = req.body.details;
-}); */
+app.post('api/events/', (req, res) => {
+  const { name } = req.body.name;
+  const { details } = req.body.details;
+  if (!name) {
+    throw new ClientError(400, 'event name is a required field');
+  }
+  if (!details) {
+    throw new ClientError(400, 'details is a required field');
+  }
+  const sql = `
+    insert into "events" ("name", "details")
+    values ($1, $2)
+    returning *;
+    `;
+  const params = [name, details];
+  db.query(sql, params)
+    .then(result => {
+      res.status(201).json(result.rows[0]);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({
+        error: 'An unexpected error occurred.'
+      });
+    });
+});
 
 app.listen(process.env.PORT, () => {
   process.stdout.write(`\n\napp listening on port ${process.env.PORT}\n\n`);
