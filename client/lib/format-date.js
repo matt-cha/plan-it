ff6f3ba312f77750a27d22699a30e27c2cff1161
Matt Cha <mcha002@ucr.edu>
2023-03-01 20:11:33 +0000

    working on twilio initial 12pm

--- client/lib/format-date.js
+++ client/lib/format-date.js
@@ -1,4 +1,4 @@
 module.exports = function formatDate(string) {
-   var options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', hour: 'numeric', minute: 'numeric' };
-    return new Date(string).toLocaleDateString('en-US',options);
-}
+  const options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', hour: 'numeric', minute: 'numeric' };
+  return new Date(string).toLocaleDateString('en-US', options);
+};
