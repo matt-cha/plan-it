4213d03aae2bc9c360bbe8e4382b369825f11034
Matt Cha <mcha002@ucr.edu>
2023-03-09 23:10:10 +0000

    loader added

--- client/app.jsx
+++ client/app.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-/* import Home from './pages/home'; */
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/navbar';
 import CreateEvent from './pages/create-event';
 import Events from './pages/events';
 import Event from './pages/event';
 import NotFound from './pages/not-found';
+import Loader from './components/loader';
 
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 0);
+  }, [location]);
 
   return (
     <div className='App '>
       <Navbar />
       <div className='pt-24'>
+        {loading && (
+          <Loader />
+        )}
         <Routes>
-          {/* <Route path='/' element={ <Home />} /> */}
           <Route path='/' element={<CreateEvent />} />
           <Route path='/events' element={<Events />} />
           <Route path='/events/:eventId' element={<Event />} />
